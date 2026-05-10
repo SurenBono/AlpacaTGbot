c291392b89a3c36ec1827dd804a0ed2ea4ae78f9
Suren Bono <surenbono76@gmail.com>
2026-05-10 13:58:56 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ sudo apt-get -y upgrade
 sudo apt install -y python3 python3-pip python3-venv
 mkdir ~/bot && cd ~/bot
 python3 -m venv venv
-source venv/bin/activate
+
 pip install alpaca-trade-api python-dotenv requests flask pandas numpy
 wget https://raw.githubusercontent.com/SurenBono/AlpacaTGbot/main/emabot.py -O ~/bot/emabot.py
 # Step 5 - Create your .env file
@@ -12,4 +12,5 @@ wget https://raw.githubusercontent.com/SurenBono/AlpacaTGbot/main/.env -O ~/bot/
 nano .env 
 # Paste your credentials, then Ctrl+X → Y → Enter to save
 # Step 6 - Run the bot
-python3 emabot.py
+source venv/bin/activate
+echo "y" | python3 emabot.py
